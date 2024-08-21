107b2e3f065d24f4170d125ca8db3d74157a4d78
Frederico Pitol <freirefrederico1@gmail.com>
2024-08-21 13:42:16 -0300

    Implementing client list

--- ServiceOrderManagerV3/Controllers/ClientsController.cs
+++ ServiceOrderManagerV3/Controllers/ClientsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ServiceOrderManagerV3.Data;
 using ServiceOrderManagerV3.Models;
 using ServiceOrderManagerV3.Models.Entities;
@@ -8,45 +9,43 @@ namespace ServiceOrderManagerV3.Controllers
 {
     public class ClientsController : Controller
     {
-        // Criado pelo create assign, nao e necessario estanciar objeto, a injecao de dependencia faz esse trabalho
+       
         private readonly AppDbContext dbContext;
 
-        // Injetando db context, apos fazer o construtor clicamos na var dbContext -> ctrl + . -> Create and assign this field
         public ClientsController(AppDbContext dbContext)
         {
             this.dbContext = dbContext;
         }
 
-        // Adiciona novo cliente
         [HttpGet]
         public IActionResult Add()
         {
             return View();
         }
-        // Metodo Que acessa o formulario no navegador e envia as informacoes 
-        // Adiciona uma nova classe no dir models add
-        //
+     
         [HttpPost]
         public async Task<IActionResult> Add(AddClientViewModel viewModel)
         {
-            //Objeto que sera enviado para o banco de dados
             var client = new Client
             {
-                // Nao necessita do id devido ao entity framework
                 Name = viewModel.Name,
                 Cnpj = viewModel.Cnpj,
                 Email = viewModel.Email,
                 Address = viewModel.Address
-
             };
-
-            // Adiciona o cliente no banco de dados
             await dbContext.Clients.AddAsync(client);
             await dbContext.SaveChangesAsync();
-
             return View();
         }
-        // Trazemos as informacoes do formulario da view para memoria temporaria do programa para enviar para o banco de dados
-        // Para isso precismos injetar a dbcontext no controller, vamos criar um construtor para isso(ClientsController)
+        //1. Fazer nova pagina com lista dos clientes adicionados
+        //2. Fazer a rota para nova pagina
+        //3. Fazer a action para nova pagina
+        [HttpGet]
+         public async Task <IActionResult> List()
+        {
+            var client = await dbContext.Clients.ToListAsync();
+
+            return View(client);
+        }
     }
 }
--- ServiceOrderManagerV3/Program.cs
+++ ServiceOrderManagerV3/Program.cs
@@ -4,38 +4,28 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
-// Injetando dependencia do db context options do arquivo AppDbCOntext
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ServiceOrderManagerV3")));
 
-// (options => options.UseSqlServer == Usanda metodos do options como argumento que herda do entity framework
-// Metodo use sql server usa sql server == Recebe uma string de conexao definida no appsetting (Examamente mesmo nome )
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthorization();
 
-// Valores padrao das rotas
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-// Por padrao o controller ira olhar para o metodos index
-// o metodo  index ira olhar para a view padra (index )usando o nome nome do controller
-// == nome dir em views que tera uma view correspondente ao nome do metodo no controller
-
 
 app.Run();
