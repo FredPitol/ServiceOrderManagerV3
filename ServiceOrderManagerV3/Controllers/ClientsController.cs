57053cf09e09acb37290302e1c550f8c93295968
Frederico Pitol <freirefrederico1@gmail.com>
2024-08-21 10:35:27 -0300

    Client view

---
+++ ServiceOrderManagerV3/Controllers/ClientsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceOrderManagerV3.Data;
+using ServiceOrderManagerV3.Models;
+
+namespace ServiceOrderManagerV3.Controllers
+{
+    public class ClientsController : Controller
+    {
+        private readonly AppDbContext dbContext;
+
+        public ClientsController(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        // Adiciona novo cliente
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        // Metodo Que acessa o formulario no navegador e envia as informacoes 
+        // Adiciona uma nova classe no dir models add
+        //
+        [HttpPost]
+        public IActionResult Add(AddClientViewModel viewModel)
+        {
+            return View();
+        }
+    }
+}
---
+++ ServiceOrderManagerV3/Models/AddClientViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ServiceOrderManagerV3.Models
+{
+    public class AddClientViewModel
+    {
+        public string Cnpj { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
